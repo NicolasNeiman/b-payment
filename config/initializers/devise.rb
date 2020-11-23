3afe5e2c2de5d741271884097408779f9107f4f8
Joanna Chaillou <joanna.chaillou@edu.escp.eu>
2020-11-23 18:46:41 +0100

    initializing

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -271,7 +271,7 @@ Devise.setup do |config|
   # ==> OmniAuth
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
-  # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
+  config.omniauth :paypal_openid, 'ASXbunSMf3ZlafxNle21wV5W0k9jcHXkj5MdaalfVtAeUK23JG_5gPptN_5TNR0YOEN5AROdUS6No7-B', 'EDDgKCckKMtnJyRtRSpmZbkTTerd0UR14Zon6KUVpbUsjZh1iQnJ8E6VuAUaE2zn8ZGLgicz5Fv5fb-z'
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
