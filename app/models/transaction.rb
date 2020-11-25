444ab6ca65cd1723f25485dec61b488e07f202d7
Thibault Sordet <sordetthibault@gmail.com>
2020-11-25 15:51:15 +0100

    fix the validation plus add monetize for bitcoin_amount

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,5 +1,6 @@
 class Transaction < ApplicationRecord
   belongs_to :user, dependent: :destroy
   monetize :amount_cents
-  validates :amount_cents, presence: true
+  monetize :bitcoin_amount_cents
+  validates :bitcoin_amount_cents, :amount_cents, presence: true
 end
