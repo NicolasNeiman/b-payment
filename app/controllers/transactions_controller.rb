1f79d77ed13c6b0280a3e102a14952f778756b3b
Joanna Chaillou <69638927+Joannacha@users.noreply.github.com>
2020-11-25 15:44:23 +0100

    deleted a space

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,5 +1,5 @@
 class TransactionsController < ApplicationController
   def index
-    @transactions = Transaction.where( user: current_user)
+    @transactions = Transaction.where(user: current_user)
   end
 end
