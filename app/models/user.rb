48d94cd8391fe5ec1840276ac5c8b3b2659beefa
Joanna Chaillou <joanna.chaillou@edu.escp.eu>
2020-11-25 17:35:31 +0100

    signing up in database connecting to wallet after

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,19 +4,4 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :omniauthable, omniauth_providers: %i[coinbase]
-
-  def self.from_omniauth(auth)
-    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-      user.email = auth.extra.raw_info.email
-      user.password = Devise.friendly_token[0, 20]
-      user.name = auth.info.name
-      user.coinbase_token = auth.credentials.token
-      user.coinbase_token_expires = auth.credentials.expires
-      user.coinbase_token_expires_at = auth.credentials.expires_at
-      user.coinbase_refresh_token = auth.credentials.refresh_token
-      # If you are using confirmable and the provider(s) you use validate emails,
-      # uncomment the line below to skip the confirmation emails.
-      # user.skip_confirmation!
-    end
-  end
 end
