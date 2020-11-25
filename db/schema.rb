a25a50a89871a465a50ec013d68c2f6d1ec82393
Thibault Sordet <sordetthibault@gmail.com>
2020-11-25 15:57:58 +0100

    add url

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2020_11_25_141706) do
+ActiveRecord::Schema.define(version: 2020_11_25_145606) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +23,7 @@ ActiveRecord::Schema.define(version: 2020_11_25_141706) do
     t.string "amount_currency", default: "USD", null: false
     t.integer "bitcoin_amount_cents", default: 0, null: false
     t.string "bitcoin_amount_currency", default: "USD", null: false
+    t.string "url"
     t.index ["user_id"], name: "index_transactions_on_user_id"
   end
 
