079a6ee810ded0fc88f235d3cf33af46cfcc3fbd
Joanna Chaillou <joanna.chaillou@edu.escp.eu>
2020-11-26 15:13:18 +0100

    route + link in navbar

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :transactions, only: [:index]
   get 'profile', to: 'pages#profile'
+  get 'dashboard', to: 'pages#dashboard'
 
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
