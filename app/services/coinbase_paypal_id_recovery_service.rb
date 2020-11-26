001fb5c529b823f8b734d04cccecce9b8277a834
Nicolas Neiman <nicolasneiman@gmail.com>
2020-11-26 11:39:29 +0100

    add paypal account id column to user table and create a service to populate it as user connects its coinbase account

--- app/services/coinbase_paypal_id_recovery_service.rb
+++ app/services/coinbase_paypal_id_recovery_service.rb
@@ -4,15 +4,21 @@ class CoinbasePaypalIdRecoveryService < ApplicationService
   end
 
   def call
+    @user.coinbase_paypal_account_id = get_paypal_id(@user)
+    @user.save
+  end
+
+  private
+
+  def get_paypal_id(user)
     url = "https://api.coinbase.com/v2/payment-methods"
     headers = {
       "Content-Type"  => "application/json",
-      "Authorization" => "Bearer #{@user.coinbase_token}"
+      "Authorization" => "Bearer #{user.coinbase_token}"
     }
     payment_methods = HTTParty.get(url, :headers => headers)["data"]
     paypal_accounts = payment_methods.select { |payment_method| payment_method["type"] == "paypal_account"}
     paypal_account = paypal_accounts.first
-    @user.coinbase_paypal_account = paypal_account
-    @user.save
+    return paypal_account["id"]
   end
 end
