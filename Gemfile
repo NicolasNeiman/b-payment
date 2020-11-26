92ac47f5a8ba235e9491c59a9e4fe726c719073f
Thibault Sordet <sordetthibault@gmail.com>
2020-11-26 12:24:43 +0100

    add the api endpoint for check connection

--- Gemfile
+++ Gemfile
@@ -35,6 +35,7 @@ gem 'autoprefixer-rails'
 gem 'font-awesome-sass'
 gem 'simple_form'
 gem 'faker'
+gem 'simple_token_authentication'
 
 group :development, :test do
   gem 'pry-byebug'
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_many :transactions, dependent: :destroy
+  acts_as_token_authenticatable
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and
   devise :database_authenticatable, :registerable,
