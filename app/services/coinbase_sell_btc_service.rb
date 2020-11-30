48f379f65c4109423fce495ce8a691ba342b802a
Nicolas Neiman <nicolasneiman@gmail.com>
2020-11-30 18:49:30 +0100

    services-answer-with-succeess-or-failure

--- app/services/coinbase_sell_btc_service.rb
+++ app/services/coinbase_sell_btc_service.rb
@@ -5,14 +5,23 @@ class CoinbaseSellBtcService < ApplicationService
   end
 
   def call
-    sell(@sell_amount)
+    res = sell(@sell_amount)
+    begin
+      success_transfer_amount = res["data"]["total"]["amount"].to_i
+    rescue
+      success_transfer_amount = nil
+    end
+    if success_transfer_amount == @withdraw_amount.to_i
+      return { "success" => "#{@withdraw_amount} EUR was withdraw and sent to your PayPal account" }
+    else
+      return {"error" => "We didn't manage to transfer #{@withdraw_amount} EUR to your account"}
   end
 
   private
 
   def sell(sell_amount)
     url = "https://api.coinbase.com/v2/accounts/#{@user.coinbase_btc_account_id}/sells"
-    HTTParty.post(url,
+    res = HTTParty.post(url,
       headers: {
         "Content-Type"  => "application/json",
         "Authorization" => "Bearer #{@user.coinbase_token}"
@@ -23,5 +32,6 @@ class CoinbaseSellBtcService < ApplicationService
         "payment_method" => @user.coinbase_eur_payment_method_id
       }.to_json
     )
+    return res
   end
 end
