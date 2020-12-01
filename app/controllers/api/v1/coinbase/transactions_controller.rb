96ddd1474b541ea28e8525860d18df0139ba97f0
Nicolas Neiman <nicolasneiman@gmail.com>
2020-12-01 14:36:43 +0100

    use API status code

--- app/controllers/api/v1/coinbase/transactions_controller.rb
+++ app/controllers/api/v1/coinbase/transactions_controller.rb
@@ -1,22 +1,31 @@
 class Api::V1::Coinbase::TransactionsController < Api::V1::BaseController
-  
   def sell
     user = User.find_by!(authentication_token: transaction_params[:token])
     sell_amount = transaction_params[:difference]
-    response = CoinbaseSellBtcService.call(user, sell_amount)
-    render json: { response: response }, status: :ok
+    coinbase_sell_btc_service = CoinbaseSellBtcService.new(user, sell_amount)
+    coinbase_sell_btc_service.call
+    if coinbase_sell_btc_service.success?
+      render json: { response: {"status" => "succes"} }, status: :ok
+    else
+      render json: { response: {"status" => "error"} }, status: :bad_request
+    end
   end
 
   def withdraw
     user = User.find_by!(authentication_token: transaction_params[:token])
     withdraw_amount = transaction_params[:price]
-    response = CoinbaseWithdrawService.call(user, withdraw_amount)
-    render json: { response: response }, status: :ok
+    coinbase_withdraw_service = CoinbaseWithdrawService(user, withdraw_amount)
+    coinbase_withdraw_service.call
+    if coinbase_withdraw_service.success?
+      render json: { response: {"status" => "succes"} }, status: :ok
+    else
+      render json: { response: {"status" => "error"} }, status: :bad_request
+    end
   end
 
   private
 
   def transaction_params
-    params.permit(:difference, :price, :token )
+    params.permit(:difference, :price, :token)
   end
 end
