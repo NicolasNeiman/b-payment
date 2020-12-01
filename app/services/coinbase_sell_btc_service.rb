5206ca4b2207a2e2d9778c847d9169b9fa429d3a
Nicolas Neiman <nicolasneiman@gmail.com>
2020-12-01 12:34:44 +0100

    Service don't answer anything use sccess method to check status

--- app/services/coinbase_sell_btc_service.rb
+++ app/services/coinbase_sell_btc_service.rb
@@ -6,40 +6,41 @@ class CoinbaseSellBtcService < ApplicationService
   end
 
   def call
-    res = sell(@sell_amount)
     begin
-      success_transfer_amount = res["data"]["total"]["amount"].to_i
+      coinbase_api_answer = HTTParty.post(
+        url,
+        headers: headers,
+        body: body
+      )
     rescue
-      success_transfer_amount = nil
+      coinbase_api_answer = {}
     end
-    if success_transfer_amount == @sell_amount.to_i
-      return {
-                "status" => "success",
-                "message" => "We successfully sold #{@sell_amount} EUR"
-             }
-    else
-      return {
-                "status" => "error",
-                "message" => "We didn't manage to sell #{@sell_amount} EUR"
-             }
-    end
+
+    @sold_amount = coinbase_api_answer.dig("data", "total", "amount")
+  end
+
+  def success?
+    !@sold_amount.nil?
   end
 
   private
 
-  def sell(sell_amount)
-    url = "https://api.coinbase.com/v2/accounts/#{@user.coinbase_btc_account_id}/sells"
-    res = HTTParty.post(url,
-      headers: {
-        "Content-Type"  => "application/json",
-        "Authorization" => "Bearer #{@user.coinbase_token}"
-      },
-      body: {
-        "total" => sell_amount,
-        "currency" => "EUR",
-        "payment_method" => @user.coinbase_eur_payment_method_id
-      }.to_json
-    )
-    return res
+  def url
+    "https://api.coinbase.com/v2/accounts/#{@user.coinbase_btc_account_id}/sells"
+  end
+
+  def headers
+    {
+      "Content-Type"  => "application/json",
+      "Authorization" => "Bearer #{@user.coinbase_token}"
+    }
+  end
+
+  def body
+    {
+      "total" => sell_amount,
+      "currency" => "EUR",
+      "payment_method" => @user.coinbase_eur_payment_method_id
+    }.to_json
   end
 end
