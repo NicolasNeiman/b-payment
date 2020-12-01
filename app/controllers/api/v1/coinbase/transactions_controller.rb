4053061c0759be60c914c1bf4faa3f440405b271
Joanna Chaillou <joanna.chaillou@edu.escp.eu>
2020-12-01 11:18:11 +0100

    ça marche sa mère

--- app/controllers/api/v1/coinbase/transactions_controller.rb
+++ app/controllers/api/v1/coinbase/transactions_controller.rb
@@ -3,15 +3,15 @@ class Api::V1::Coinbase::TransactionsController < Api::V1::BaseController
   def sell
     user = User.find_by!(authentication_token: transaction_params[:token])
     sell_amount = transaction_params[:difference]
-    message = CoinbaseSellBtcService.call(user, sell_amount)
-    render json: { message: message }, status: :ok
+    response = CoinbaseSellBtcService.call(user, sell_amount)
+    render json: { response: response }, status: :ok
   end
 
   def withdraw
     user = User.find_by!(authentication_token: transaction_params[:token])
     withdraw_amount = transaction_params[:price]
-    message = CoinbaseWithdrawService.call(user, withdraw_amount)
-    render json: { message: message }, status: :ok
+    response = CoinbaseWithdrawService.call(user, withdraw_amount)
+    render json: { response: response }, status: :ok
   end
 
   private
