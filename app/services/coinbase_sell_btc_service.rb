afa2244a0f0569ef2659d3b58c415b50e9bcc8b0
Joanna Chaillou <joanna.chaillou@edu.escp.eu>
2020-12-01 10:43:54 +0100

    still correcting les erreurs de Nico

--- app/services/coinbase_sell_btc_service.rb
+++ app/services/coinbase_sell_btc_service.rb
@@ -13,9 +13,9 @@ class CoinbaseSellBtcService < ApplicationService
       success_transfer_amount = nil
     end
     if success_transfer_amount == @withdraw_amount.to_i
-      return { "success" => "#{@withdraw_amount} EUR were withdrawn and sent to your PayPal account" }
+      return { "success" => "#{@sell_amount} EUR were withdrawn and sent to your PayPal account" }
     else
-      return { "error" => "Sorry, we didn't manage to transfer #{@withdraw_amount} EUR to your account" }
+      return { "error" => "Sorry, we didn't manage to transfer #{@sell_amount} EUR to your account" }
     end
   end
 
